b180a9892c87f906bad14add3923625bbdc5bacc
Brad Abrams <bma@google.com>
2014-04-07 01:22:47 -0700

    local edit

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world! edit from github')
+        self.response.write('Hello world! edit from github -- edit locally')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
